c20232d70f12a4687824e758317f8089e02868e0
varFox <31412438+varFox@users.noreply.github.com>
2019-04-14 19:30:00 +0300

    55

--- js/svgg.js
+++ js/svgg.js
@@ -1,7 +1,7 @@
-const arrow = '<svg height="40px" id="Layer_1" style="" version="1.1" viewBox="0 0 128 128" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line style="fill:none;stroke:#ff5300;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="40.5" x2="87.5" y1="17" y2="64"/><line style="fill:none;stroke:#ff5300;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="87.5" x2="40.5" y1="64" y2="111"/></g></svg>';
+// const arrow = '<svg height="40px" id="Layer_1" style="" version="1.1" viewBox="0 0 128 128" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line style="fill:none;stroke:#ff5300;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="40.5" x2="87.5" y1="17" y2="64"/><line style="fill:none;stroke:#ff5300;stroke-width:12;stroke-linecap:square;stroke-miterlimit:10;" x1="87.5" x2="40.5" y1="64" y2="111"/></g></svg>';
 
-document.querySelectorAll('.arrow').forEach(item => {
-  item.innerHTML = arrow;
-});
+// document.querySelectorAll('.arrow').forEach(item => {
+//   item.innerHTML = arrow;
+// });
 
 
